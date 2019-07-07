6b98d6074aad2c0de7ea8b69a7c51658e3a56cfd
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-07-07 15:00:50 -0600

    html I was able to do

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
 console.log("hey");
+
+var oneBtn = document.getElementById("numberOne");
+var twoBtn = document.getElementById("numberTwo");
+var threeBtn = document.getElementById("numberThree");
+var fourBtn = document.getElementById("numberFour");
+var fiveBtn = document.getElementById("numberFive");
+var sixBtn = document.getElementById("numberSix");
+var sevenBtn = document.getElementById("numberSeven");
+var eightBtn = document.getElementById("numberEight");
+var nineBtn = document.getElementById("numberNine");
+var zeroBtn = document.getElementById("numberZero");
+var decimalBtn = document.getElementById("decimal")
+var clearBtn = document.getElementById("clear");
+var displayElement = document.getElementById("display")
+
+
+
+
+
+
+
+
+
+
+
+
+
+     //function that display value 
+         // function dis(val) 
+         // { 
+         //     document.getElementById("result").value+=val 
+         // } 
+           
+         // //function that evaluates the digit and return result 
+         // function solve() 
+         // { 
+         //     let x = document.getElementById("result").value 
+         //     let y = eval(x) 
+         //     document.getElementById("result").value = y 
+         // } 
+           
+         // //function that clear the display 
+         // function clr() 
+         // { 
+         //     document.getElementById("result").value = "" 
+         // } 
